27c525945b44855bb002b138b601f72bf6e048ca
geckoflume <geckoflume@tuta.io>
2018-10-13 19:28:48 +0000

    Changed 'description' type from string to text

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -62,7 +62,7 @@ class Event
     private $time_end;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      */
     private $description;
 
